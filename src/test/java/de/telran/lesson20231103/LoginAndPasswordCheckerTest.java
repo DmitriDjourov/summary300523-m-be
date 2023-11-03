c92c5f88828edd0cf258faec7fb89541a17adbf3
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2023-11-03 11:18:43 +0100

    maven support

---
+++ src/test/java/de/telran/lesson20231103/LoginAndPasswordCheckerTest.java
@@ -0,0 +1,30 @@
+package de.telran.lesson20231103;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class LoginAndPasswordCheckerTest {
+
+    private static LoginAndPasswordChecker checker;
+
+    @BeforeAll
+    public static void init(){
+        checker = new LoginAndPasswordChecker();
+    }
+
+    @Test
+    public void checkLogin(){
+
+    }
+
+    @Test
+    public void checkPassword(){
+
+    }
+
+    @Test
+    public void checkConfirmPassword(){
+
+    }
+
+}
