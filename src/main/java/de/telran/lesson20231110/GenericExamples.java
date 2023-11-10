8098d0aab66339ce5ba729965e6d16739cc9f26f
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2023-11-10 16:50:42 +0100

    generics

--- src/main/java/de/telran/lesson20231110/GenericExamples.java
+++ src/main/java/de/telran/lesson20231110/GenericExamples.java
@@ -3,14 +3,12 @@ package de.telran.lesson20231110;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 
 public class GenericExamples {
 
     public static void main(String[] args) {
         List list = Arrays.asList(1, 2, 3);
         method(list);
-
 
         List<Integer> integerData = Arrays.asList(10, 20);
         List<Double> doubleData = Arrays.asList(10.0, 20.0);
@@ -20,17 +18,22 @@ public class GenericExamples {
         processNumber(integerData);
         processNumber(doubleData);
 
+//        processNumber2(integerData);
+        List<Number> numbers = Arrays.asList(10, 20.0);
+        processNumber2(numbers);
+
         List<? super Number> data2;
         List<Number> data3;
 
 
         // PECS = Producer Extends Consumer Super
 
-        // PECS = Producer Extends
-        List<BMW> bmws = new ArrayList<>();
-        List<Volwo> volwos = new ArrayList<>();
+        // Producer Extends Example
         List<Car> cars = new ArrayList<>();
+        List<Volwo> volwos = new ArrayList<>();
+        List<BMW> bmws = new ArrayList<>();
         List<WhiteBMW> whiteBMWs = new ArrayList<>();
+        whiteBMWs.add(new WhiteBMW());
 
         List<? extends Car> carsInShop = bmws;
         carsInShop = volwos;
@@ -43,17 +46,16 @@ public class GenericExamples {
 
         Car car = carsInShop.get(0); // Producer
 
-
-        // Consumer Super
+        // Consumer Super Example
         List<? super Car> carsOnTheParking = new ArrayList<>();
         carsOnTheParking.add(new Car()); // Consumer
         carsOnTheParking.add(new BMW());
         carsOnTheParking.add(new Volwo());
 //        carsOnTheParking.add(new Object());
 
-        Object o = carsOnTheParking.get(0);
+        Object o = carsOnTheParking.get(0); // only Object type of variable possible
 
-        List<? super BMW> carsList = new ArrayList<>();
+        List<? super BMW> carsList;
         carsList = new ArrayList<BMW>();
         carsList = new ArrayList<Car>();
         carsList = new ArrayList<Object>();
@@ -66,6 +68,9 @@ public class GenericExamples {
     }
 
     public static void processNumber(List<? extends Number> data) {
+    }
+
+    public static void processNumber2(List<Number> data) {
     }
 
 
--- src/main/java/de/telran/lesson20231110/UseMySteam.java
+++ src/main/java/de/telran/lesson20231110/UseMySteam.java
@@ -1,11 +1,11 @@
 package de.telran.lesson20231110;
 
-public class UseSteams {
+public class UseMySteam {
 
     public static void main(String[] args) {
         MyStream<String> myStream = new MyStream<>();
 
-        MyStream<String> filtered = myStream.filter(s -> s.length() == 3).parallel().filter(s -> s == "A");
+        MyStream<String> filtered = myStream.filter(s -> s.length() == 3).parallel().filter(s -> s.equals("A"));
 
 
     }
