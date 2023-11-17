55febbfa59573a4e8f315d660593fb511cfea3b8
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2023-11-17 11:57:08 +0100

    generics, reflection

---
+++ src/main/java/de/telran/lesson20231117/Processor.java
@@ -0,0 +1,24 @@
+package de.telran.lesson20231117;
+
+import java.util.UUID;
+
+public class Processor {
+
+    public int count = 0;
+
+    private String id;
+
+
+    public Processor(String id) {
+        this.id = id;
+    }
+
+    public void process(int data){
+        System.out.println("Processing " + data);
+    }
+
+    private void changeId(){
+        id = UUID.randomUUID().toString();
+    }
+
+}
